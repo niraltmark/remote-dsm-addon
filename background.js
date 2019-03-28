769d957dd340d977383edb81cfea60875d413992
Nir Altmark <niraltmark@gmail.com>
2019-03-28 17:40:46 +0200

    Fixing errors and skipping days

--- background.js
+++ background.js
@@ -6,28 +6,36 @@
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.alarms.create("remote-dsm", {
-    when: new Date('3/27/2019 10:28:00').getTime()
+    when: Date.now() + 1000
   });
 });
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
-  chrome.windows.create({
-    url: 'https://meet.google.com/okq-fhzh-khm'
-  }, function (window) {
-    chrome.tabs.getSelected(null, function (tab) {
-      chrome.tabs.executeScript(tab.id, {
-        file: 'inject.js'
+  let now = new Date(Date.now());
+
+  // Meeting should only happen in Sunday, Monday, Tuesday, Wednesday
+  if (now.getDay() < 4) { 
+    chrome.windows.create({
+      url: 'https://meet.google.com/okq-fhzh-khm'
+    }, function (window) {
+      chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.executeScript(tab.id, {
+          file: 'inject.js'
+        });
       });
     });
-  });
+  }
   
   chrome.alarms.clear("remote-dsm", function() {
     let now = new Date(Date.now());
-    let meetingDateAndTime = new Date(now.getFullYear(), now.getMonth(), now.getDay(), 10, 28);
-    let nextMeetingDateAndTime = new Date(meetingDateAndTime.getDate() + 1);
+
+    // Alert should not be trigger on Thursday, Friday, Saterday 
+    let daysToAdd = (now.getDay() < 3) ? 1 : 7 - now.getDay();
+
+    let meetingDateAndTime = new Date(now.getFullYear(), now.getMonth(), now.getDate() + daysToAdd, 10, 28);
 
     chrome.alarms.create("remote-dsm", {
-      when: nextMeetingDateAndTime.getTime()
+      when: meetingDateAndTime.getTime()
     });
   });
 });
